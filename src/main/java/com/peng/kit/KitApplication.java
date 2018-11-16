51fb0e06b184271ef6c209c4bc168817d02089cf
郭清朋mac <gpl_family@163.com>
2018-11-17 01:00:24 +0800

    初建项目

---
+++ src/main/java/com/peng/kit/KitApplication.java
@@ -0,0 +1,9 @@
+package com.peng.kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitApplication {
+    public static void main(String[] args) { SpringApplication.run(KitApplication.class,args); }
+}
