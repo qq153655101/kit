0a63086d55046b3293039d84c75622d25b995e97
郭清朋mac <gpl_family@163.com>
2019-03-20 18:33:47 +0800

    增加栅栏并发执行获取结果工具---添加注释和日志，以及构造方法

--- peng-util/src/main/java/com/peng/kit/async/AsyBarrierLauncher.java
+++ peng-util/src/main/java/com/peng/kit/async/AsyBarrierLauncher.java
@@ -1,6 +1,7 @@
 package com.peng.kit.async;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StopWatch;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +12,8 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
- *
- *
+ * 利用栅栏协调一组线程处理任务
+ * 注意：对于处理用一个对象（getOriginal，result）的同一个成员变量可能存在线程不安全
  * created by guoqingpeng on 2019/3/20
  */
 @Slf4j
@@ -26,23 +27,56 @@ public class AsyBarrierLauncher<T,R> {
     private T original;
     private R result;
 
-    public void doTaskW(){
+
+    public AsyBarrierLauncher(Executor executor, long timeOut) {
+        this.executor = executor;
+        this.timeOut = timeOut;
+    }
+
+    /**
+     * 使用CyclicBarrier协调一组线程处理任务，若处理的是同一个对象数据可用getOriginal获取结果
+     */
+    public void doTask(){
+        if (consumers == null || resultFunction ==null)
+            throw new RuntimeException("resultFunction or runnable list is null");
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("AsyBarrierLauncher doTaskW");
         cyclicBarrier = new CyclicBarrier(consumers.size()+1);
         this.execute();
         this.waitToFinish();
+        stopWatch.stop();
+        this.stopWatchLog(stopWatch);
     }
 
+    /**
+     * 使用CyclicBarrier协调一组线程处理对象original，并经过mergeConsumer最终转换成result
+     */
     public void doTaskWithMerge(){
+        if  (consumers == null || mergeConsumer ==null)
+            throw new RuntimeException("mergeConsumer or runnable list is null");
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("AsyBarrierLauncher doTaskWithMerge");
         cyclicBarrier = new CyclicBarrier(consumers.size()+1,()->mergeConsumer.accept(result));
         this.execute();
         this.waitToFinish();
+        stopWatch.stop();
+        this.stopWatchLog(stopWatch);
     }
 
+    /**
+     * 使用CyclicBarrier协调一组线程处理对象original，并经过resultFunction最终转换成result
+     */
     public void doTaskWithFunction(){
+        if  (consumers == null)
+            throw new RuntimeException("runnable list is null");
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("AsyBarrierLauncher doTaskWithFunction");
         cyclicBarrier = new CyclicBarrier(consumers.size()+1);
         this.execute();
         this.waitToFinish();
         this.result = resultFunction.apply(original);
+        stopWatch.stop();
+        this.stopWatchLog(stopWatch);
     }
 
     public void execute(){
@@ -72,6 +106,10 @@ public class AsyBarrierLauncher<T,R> {
         return this;
     }
 
+    public void stopWatchLog(StopWatch stopWatch){
+        log.info(stopWatch.prettyPrint());
+    }
+
     public T getOriginal() {
         return original;
     }
@@ -80,8 +118,8 @@ public class AsyBarrierLauncher<T,R> {
         return result;
     }
 
-    public AsyBarrierLauncher() {
-    }
+    //对外不提供默认构造方法
+    private AsyBarrierLauncher() { }
 
     public AsyBarrierLauncher setExecutor(Executor executor) {
         this.executor = executor;
